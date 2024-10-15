579cfeff458390cbeea3a07d46a106fc5ef48bf9
Daniel Calle <daniel.calle@wsu.edu>
2024-10-15 14:37:19 -0700

    ADD: Menu UI setup with buttons for different pages we need to implement.
    These buttons are tentative and up for deliberation on whether we need all of them, need extra, etc. Also added a Resources folder for icons and other things used in the WinForms application,

---
+++ RoomMate Application/RoomMate_WinFormsApp/Form1.cs
@@ -0,0 +1,107 @@
+using System.Runtime.InteropServices;
+
+namespace RoomMate_WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private Button currentButton;
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+
+            currentButton = btnDashboard;
+            ActivateButton(btnDashboard);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDashboard.Focus();
+        }
+
+        private void ActivateButton(Button button)
+        {
+            if (currentButton != null)
+            {
+                DeactivateButton(currentButton);
+            }
+
+            currentButton = button;
+            PnlNav.Height = button.Height;
+            PnlNav.Top = button.Top;
+            PnlNav.Left = button.Left;
+            button.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void DeactivateButton(Button button)
+        {
+            button.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            ActivateButton(btnDashboard);
+        }
+
+        private void btnMessages_Click(object sender, EventArgs e)
+        {
+            ActivateButton(btnMessages);
+        }
+
+        private void btnMatches_Click(object sender, EventArgs e)
+        {
+            ActivateButton(btnMatches);
+        }
+
+        private void btnProfile_Click(object sender, EventArgs e)
+        {
+            ActivateButton(btnProfile);
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            ActivateButton(btnSettings);
+        }
+
+        private void btnDashboard_Leave(object sender, EventArgs e)
+        {
+            DeactivateButton(btnDashboard);
+        }
+
+        private void btnMessages_Leave(object sender, EventArgs e)
+        {
+            DeactivateButton(btnMessages);
+        }
+
+        private void btnMatches_Leave(object sender, EventArgs e)
+        {
+            DeactivateButton(btnMatches);
+        }
+
+        private void btnProfile_Leave(object sender, EventArgs e)
+        {
+            DeactivateButton(btnProfile);
+        }
+
+        private void btnSettings_Leave(object sender, EventArgs e)
+        {
+            DeactivateButton(btnSettings);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
