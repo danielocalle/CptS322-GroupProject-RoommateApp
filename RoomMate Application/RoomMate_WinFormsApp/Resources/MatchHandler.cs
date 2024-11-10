750755548165ca89d60c911423241bea5448968e
Astera Tuhina <astera.tuhina@wsu.edu>
2024-11-09 17:43:19 -0800

    MatchHandler feature

---
+++ RoomMate Application/RoomMate_WinFormsApp/Resources/MatchHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomMate_WinFormsApp.Resources
+{
+    internal class MatchHandler
+    {
+    }
+}
